38cc595b9e2a9f11ac5af64eb157044ce5990598
root <root@isthisilham.localdomain>
2025-05-31 15:37:59 +0700

    add descrip in backup.sh

--- script/backup.sh
+++ script/backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Tentukan folder yang ingin dibackup
 FOLDER=~/devops-latihan
+# Tentukan nama file hasil backup
 DEST=~/devops-latihan/backup.sh-$(date +%F).tar.gz
 
 echo "Membackup folder $FOLDER ke $DEST"
 tar -czvf "$DEST" "$FOLDER"
+
