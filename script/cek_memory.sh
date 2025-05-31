5a289adc968f61fd0abb8266bef917d30e7760c5
root <root@isthisilham.localdomain>
2025-05-31 15:41:54 +0700

    refactor: pindahkan log memori ke folder logs/

--- script/cek_memory.sh
+++ script/cek_memory.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-LOGFILE=~/devops-latihan/memory-$(date +%F).log
+
+# Buat folder logs jika belum ada
+mkdir -p ~/devops-latihan/logs
+
+# Lokasi file log
+LOGFILE=~/devops-latihan/logs/memory-$(date +%F).log
 
 echo "Mengecek penggunaan memori..."
 free -h >> "$LOGFILE"
